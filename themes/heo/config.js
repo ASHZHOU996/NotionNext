5c6ee607a63bc7233a74d990fda3744765a2457a
ASHZHOU996 <haooo.zhou@gmail.com>
2025-03-04 17:24:24 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -9,7 +9,7 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { title: '欢迎来到我的博客', url: 'https://mosaicnexus.vercel.app/' },
-    { title: '访问文档中心获取更多帮助', url: 'https://mosaicnexus.vercel.app/' }
+    { title: '希望我的教程能帮助到你', url: 'https://mosaicnexus.vercel.app/' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -18,9 +18,9 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
+  HEO_HERO_TITLE_1: '分享干货',
+  HEO_HERO_TITLE_2: '不讲废话',
+  HEO_HERO_TITLE_3: '科技 软件 数码 加密货币',
   HEO_HERO_TITLE_4: '新版上线',
   HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
   HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
@@ -41,11 +41,11 @@ const CONFIG = {
   HEO_INFOCARD_GREETINGS: [
     '你好！我是',
     '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
+    '🤝 专修金融与科技',
     '🏃 脚踏实地行动派',
-    '🏠 智能家居小能手',
+    '🏠 科学上网小天才',
     '🤖️ 数码科技爱好者',
-    '🧱 团队小组发动机'
+    '🧱 BTC长期持有者'
   ],
 
   // 个人资料底部按钮
